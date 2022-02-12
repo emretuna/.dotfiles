bf6428e055e5d6a28a169269b9874bc3e6f653d8
emretuna <tunaemre@windowslive.com>
2022-02-12 04:07:13 -0800

    Create install_wsl.sh

---
+++ install_wsl.sh
@@ -0,0 +1,76 @@
+if [ ! -d ~/.nvm ]; then
+  echo "nvm install started..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+  . ${HOME}/.nvm/nvm.sh
+  . ${HOME}/.profile
+  . ${HOME}/.bashrc
+fi
+# install node
+nvm install node --lts
+
+ # install homebrew
+ 
+	 # install nix
+  curl -L https://nixos.org/nix/install | sh
+
+  # source nix
+  . ~/.nix-profile/etc/profile.d/nix.sh
+  export LOCALE_ARCHIVE="$(nix-env --installed --no-name --out-path --query glibc-locales)/lib/locale/locale-archive"
+
+  # install packages
+    nix-env -iA nixpkgs.glibcLocales\
+    nixpkgs.zsh \
+    nixpkgs.antibody \
+    nixpkgs.neovim \
+    nixpkgs.stow \
+    nixpkgs.yarn \
+    nixpkgs.fd \
+    nixpkgs.ripgrep \
+    nixpkgs.bat \
+    nixpkgs.bpytop \
+    nixpkgs.ranger
+     	
+	      
+
+		cd ${HOME}/.dotfiles
+		echo "stowing files..."
+		# stow dotfiles
+		stow git
+		stow zsh
+		stow nvim
+		stow bpytop
+		stow ranger
+
+		echo "installing zsh and setting up things..."
+		# add zsh as a login shell
+		command -v zsh | sudo tee -a /etc/shells
+
+		echo "changing default shell"
+		# use zsh as default shell
+		sudo chsh -s $(which zsh) $USER
+
+		echo "tweaking zsh..."
+		# bundle zsh plugins 
+		antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+
+		echo "started installing node packages..."
+		# install neovim plugins and requirements
+		npm i -g vscode-langservers-extracted
+		npm i -g typescript typescript-language-server
+
+		echo "finish nvim set up..."
+		nvim --headless +PlugInstall +qall
+
+
+		#continue
+		echo "Updating package lists"
+		sudo apt update
+		sudo apt install -y fzf \
+				                gcc \
+	                      make \
+                        direnv \
+                        trash-cli
+                        
+    #install rust cargo
+	curl https://sh.rustup.rs -sSf | sh
+	echo "DONE BOSS!SEE YA!"
