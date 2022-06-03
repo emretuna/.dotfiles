368dabbf13922130b4eb5b22aec7a8399fa868fb
Emre Tuna <tunaemre@windowslive.com>
2022-06-03 08:30:40 +0300

    update

---
+++ laptop_extras.sh
@@ -0,0 +1,23 @@
+echo "installing laptop tools..."
+		sudo apt install -y wmctrl \
+				 xdotool \
+				 libinput-tools \
+                 tlp \
+				 
+		sudo gpasswd -a $USER input
+
+echo "pulling libinput-gestures"
+		cd ${HOME}/Downloads
+		git clone https://github.com/bulletmark/libinput-gestures.git
+		cd libinput-gestures
+		sudo ./libinput-gestures-setup install
+
+        libinput-gestures-setup autostart start
+
+
+        stow gestures
+
+git clone https://github.com/AdnanHodzic/auto-cpufreq.git
+cd auto-cpufreq && sudo ./auto-cpufreq-installer
+
+systemctl enable auto-cpufreq
