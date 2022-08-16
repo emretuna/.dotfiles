499a5837ffcc2dcc805dd984f35f75ae4575fec4
Emre Tuna <tunaemre@windowslive.com>
2022-08-16 18:08:46 +0300

    wifi menu added to polybar

--- rofi/.config/rofi/scripts/wifimenu.sh
+++ rofi/.config/rofi/scripts/wifimenu.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
-rofi_command="rofi -theme ~/.cache/wal/wifimenu.rasi"
+THEME="$HOME/.cache/wal/wifimenu.rasi"
 notify-send "Getting list of available Wi-Fi networks..."
 # Get a list of available wifi connections and morph it into a nice-looking list
 wifi_list=$(nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/ /g" | sed "s/^--/ /g" | sed "s/  //g" | sed "/--/d")
@@ -13,7 +12,7 @@ elif [[ "$connected" =~ "disabled" ]]; then
 fi
 
 # Use rofi to select wifi network
-chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " )
+chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -theme "${THEME}" -i -selected-row 1 -p "Wi-Fi SSID: " )
 # Get name of connection
 chosen_id=$(echo "${chosen_network:3}" | xargs)
 
@@ -32,10 +31,8 @@ else
 		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
 	else
 		if [[ "$chosen_network" =~ "" ]]; then
-			wifi_password=$(rofi -dmenu -p "Password: " )
+			wifi_password=$(rofi -dmenu -theme "${THEME}" -p "Password: " )
 		fi
 		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
 	fi
 fi
-
-
